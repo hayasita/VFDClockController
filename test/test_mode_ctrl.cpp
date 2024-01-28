42fec189a8232b29d764d48aa45c10a4d2a419b5
Takahiro Hayashi <hayasita04@gmail.com>
2024-01-28 18:28:08 +0900

    [Update] getDispModeVfdのテストを追加

--- test/test_mode_ctrl.cpp
+++ test/test_mode_ctrl.cpp
@@ -13,6 +13,35 @@
 
 /**
  * @brief Construct a new TEST object
+ * 初期値確認
+ * 
+ */
+TEST(ModeCtrl, ctrlModechg_ini){
+  dispMode modeData;
+  modeCtrl mode(true,true);
+
+  // 初期値確認
+  EXPECT_EQ(mode.getCtrlMode(), ctrlMode_VfdDisp);            // 操作モード取得：VFD表示
+  EXPECT_EQ(mode.getDispModeVfd(), dispModeVfd_Default);      // VFD表示モード取得：初期値
+  EXPECT_EQ(mode.getDispModeVfdCtrl(), 0);                    // VFD設定表示モード取得：初期値
+  EXPECT_EQ(mode.getDispModeM5OLED(), dispModeOled_Default);  // M5OLED表示モード取得：初期値
+  EXPECT_EQ(mode.getDispModeOLED(), dispModeOled_Default);    // OLED表示モード取得：初期値
+
+  modeData = mode.modeSet(0,0);
+  EXPECT_EQ(modeData.ctrlModeSelect, 0);                      // 操作モード選択　0:モード切替
+  EXPECT_EQ(modeData.adjKeyData, 0);                          // 設定操作用キー情報
+  EXPECT_EQ(modeData.ctrlMode, ctrlMode_VfdDisp);             // 操作モード
+
+  EXPECT_EQ(modeData.vfdCtrlModeCount, VFD_DISP_DEFAULT);     // VFD表示モード初期化
+  EXPECT_EQ(modeData.vfdCtrlModeCount, 0);                    // VFD表示モードテーブルカウンタ
+
+  EXPECT_EQ(modeData.dispModeVfdCtrl, 0);                     //VFD設定表示モード
+  EXPECT_EQ(modeData.dispModeM5OLED, dispModeOled_Default);   // M5OLED表示モード初期化
+  EXPECT_EQ(modeData.dispModeOLED, dispModeOled_Default);     // OLED表示モード初期化
+}
+
+/**
+ * @brief Construct a new TEST object
  * CtrlMode状態遷移確認
  * OLED あり
  * M5OLED　あり
@@ -238,3 +267,42 @@ TEST(ModeCtrl, ctrlModechg_Mini){
   EXPECT_EQ(mode.getCtrlMode(), ctrlMode_VfdDisp);
 
 }
+
+TEST(ModeCtrl, modeSetVFD){
+  dispMode modeData;
+  modeCtrl mode(true,true);
+
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_DEFAULT);         // VFD表示モード取得：初期値
+
+  // UP Key Lotate
+  mode.modeSetVFD(KEY_UP_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_TIMECLOCK);       // 表示モードプラス確認
+  mode.modeSetVFD(KEY_UP_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_CALENDAR);        // 表示モードプラス確認
+  mode.modeSetVFD(KEY_UP_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_TIME_SENSOR3);    // 表示モードプラス確認
+  mode.modeSetVFD(KEY_UP_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_TMP);             // 表示モードプラス確認
+  mode.modeSetVFD(KEY_UP_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_DEFAULT);         // 表示モードローテーション確認
+
+  // DOWN Key Lotate
+  mode.modeSetVFD(KEY_DOWN_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_TMP);             //  表示モードローテーション確認
+  mode.modeSetVFD(KEY_DOWN_S,0);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_TIME_SENSOR3);    //  表示モードマイナス確認
+
+  // VFD Mode Redet
+  mode.modeSetVFD(0,SWKEY_DISP_MODE_VFD_CLR);
+  EXPECT_EQ(mode.getDispModeVfd(), VFD_DISP_DEFAULT);         // VFD表示モード初期化
+
+  // adjKeyData setKey
+  modeData = mode.modeSet(KEY_SET_S,0);
+  EXPECT_EQ(modeData.adjKeyData, KEY_SET_S);                  // 設定操作用キー情報設定 
+
+  // control mode to adj
+  modeData = mode.modeSet(kEY_SET_L,0);
+  EXPECT_EQ(modeData.ctrlMode, ctrlMode_VfdCtrl);             // 操作モード：VFD表示 -> VFD設定 
+
+}
+
